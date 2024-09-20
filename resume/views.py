8e772696a28e50a5ef4624a2a77ab4415bba8e95
NilouSaber <nelssaber@gmail.com>
2024-09-20 17:43:41 +0330

    template tags added

--- resume/views.py
+++ resume/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def cv_index(request):
-    return render(request, "index.html")
+    context = {'name_family':'Amir Shabanpour', 
+    'email':'amir.shabanpoor92@gmail.com', 
+    'birthday':'7 Feb 1992',
+    'phone': '+98 911 216 7714',
+    'city': 'Babolsar, Iran',
+    'degree': 'Master',
+    'freelance' : 'Available',
+    'age': '32'}
+    return render(request, "index.html", context)
